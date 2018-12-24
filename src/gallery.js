a46eae373e57ae61fcb3f270660b42978aa4778f
wsa <dannvx@gmail.com>
2018-12-24 03:06:19 +0200

    sketch up stuff

---
+++ src/gallery.js
@@ -0,0 +1,171 @@
+// https://stackoverflow.com/questions/6942785/window-innerwidth-vs-document-documentelement-clientwidth
+// https://bugzilla.mozilla.org/show_bug.cgi?id=156388#c14
+function getViewportHeight() {
+  // getElementsByTagName, if I'm not mistaken returns a livelist (hell knows what that is, but it's
+  // updated live - as dom gets changed). I'm not sure about using it, it behaved misteriously once...
+  // But, querySelector is not so compatible.
+  // Maybe: (document.querySelector('body') || document.getElementsByTagName('body')[0].clientWidth)
+  return window.innerHeight && document.documentElement.clientHeight ?
+    Math.min(window.innerHeight, document.documentElement.clientHeight) :
+    window.innerHeight || document.documentElement.clientHeight
+      || (document.querySelector('body') || document.getElementsByTagName('body')[0].clientHeight);
+}
+
+function getViewportWidth() {
+  return window.innerWidth && document.documentElement.clientWidth ?
+    Math.min(window.innerWidth, document.documentElement.clientWidth) :
+    window.innerWidth || document.documentElement.clientWidth
+      || (document.querySelector('body') || document.getElementsByTagName('body')[0].clientWidth);
+}
+
+class Photo {
+  constructor() {
+
+  }
+
+  loadImage(url) {
+    const img = document.createElement('img')
+
+    return new Promise((resolve, reject) => {
+      img.onload = () => {
+        resolve(img)
+      }
+
+      img.src = url
+    })
+  }
+
+  fitImage() {
+
+  }
+}
+
+class DeckItem {
+  constructor(imageUrl, options) {
+    this.el = document.createElement('div')
+    this.el.className = 'deck-item'
+
+    this.breakpoint = options.breakpoint
+
+    this.contentBasedWidth = options.contentBasedWidth
+    // this.settings = {
+    //   height: options.height
+    // }
+
+    this.loadPhoto(imageUrl)
+  }
+
+  /**
+    @param {String} height height in css syntax
+  */
+  setHeight(height) {
+    this.el.style.height = height
+  }
+
+  setWidth(width) {
+    this.el.style.width = width
+  }
+
+  calculateFitBoth(img, container) {
+    const imgDims = img.getBoundingClientRect()
+    const containerDims = container.getBoundingClientRect()
+
+    imgDims.ratio = imgDims.width / imgDims.height
+    containerDims.ratio = containerDims.width / containerDims.height
+
+    // if wider than higher
+    if (imgDims.ratio >= containerDims.ratio) {
+      const imgDimsNew = {
+        width: containerDims.width,
+        height: containerDims.width / imgDims.ratio
+      }
+
+    // if higher than wider
+    } else {
+      const imgDimsNew = {
+        // width: containerDims.height * imgDims.ratio,
+        width: containerDims.height * imgDims.ratio,
+        height: containerDims.height
+      }
+    }
+
+    return imgDimsNew
+  }
+
+  calculateFitByHeight(img, container) {
+    const imgDims = img.getBoundingClientRect()
+    const containerDims = container.getBoundingClientRect()
+
+    imgDims.ratio = imgDims.width / imgDims.height
+    const imgDimsNew = {
+      height: containerDims.height,
+      width: containerDims.height * imgDims.ratio
+    }
+
+    return imgDimsNew
+  }
+
+  fitByHeight(img) {
+    const imgDims = this.calculateFitByHeight(img, this.el)
+    img.style.width = imgDims.width
+    img.style.height = imgDims.height
+
+    return img
+  }
+
+  fitBoth(img) {
+    const imgDims = this.calculateFitByHeight(img, this.el)
+    img.style.width = imgDims.width
+    img.style.height = imgDims.height
+
+    return img
+  }
+
+  loadPhoto(url) {
+    this.loadImage() // Photo.prototype.loadImage()
+    .then((img) => {
+
+      // we don't want to see the img, but we want to be able to measure it with getBoundingClientRect (so display: none is not a fit)
+      img.style.visibility = "hidden"
+      this.el.appendChild(img)
+
+      if (getViewportWidth() > this.breakpoint) {
+        // this.fitByHeight(img)
+        const imgDims = this.calculateFitByHeight(img, this.el)
+        img.style.width = imgDims.width + 'px'
+        img.style.height = imgDims.height + 'px'
+
+        if (this.contentBasedWidth) {
+          this.el.style.width = imgDims.width + 'px'
+        }
+      } else {
+        this.fitBoth(img)
+      }
+
+      img.style.visibility = 'visible'
+    })
+  }
+}
+
+class Deck {
+  constructor() {
+    this.el = document.createElement('div')
+    this.el.className = 'gallery-deck'
+
+    this.breakpoint = options.breakpoint
+  }
+
+  setPhotos(urls) {
+    return urls.map(() => {
+      return new DeckItem(url, )
+    })
+  }
+}
+
+class Gallery() {
+  constructor() {
+
+  }
+
+
+}
