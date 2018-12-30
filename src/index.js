4d6865069a571b484342c138b05de71f3bdcdaf4
wsa <dannvx@gmail.com>
2018-12-30 23:12:18 +0200

    comment out the boot call in index js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 // import {slide} from './slide.js'
 import {boot} from './gallery.js'
 
-boot([
-  'media/in/14902841_1259112727483912_3284315106318981574_o.jpg',
-  'media/in/19875272_10100482296286706_3883306275546166676_n.jpg',
-  'media/in/3b.jpg',
-  'media/in/6a.jpg',
-  'media/in/DSC_0126.jpg',
-  'media/in/DSC_0128.JPG',
-  'media/in/DSC_0350.JPG'
-])
+// boot([
+//   'media/in/14902841_1259112727483912_3284315106318981574_o.jpg',
+//   'media/in/19875272_10100482296286706_3883306275546166676_n.jpg',
+//   'media/in/3b.jpg',
+//   'media/in/6a.jpg',
+//   'media/in/DSC_0126.jpg',
+//   'media/in/DSC_0128.JPG',
+//   'media/in/DSC_0350.JPG'
+// ])
